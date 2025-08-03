ba914049e0580e93cfd52c0110caaed8f79ea0d0
Rishabh Bhatia <ojas14@gmail.com>
2025-08-03 15:49:35 +1000

    Setup mapper to map dtos to domain models

---
+++ app/src/main/java/com/rishabh/foodiesclub/features/restaurants/data/mappers/RestaurantMappers.kt
@@ -0,0 +1,32 @@
+package com.rishabh.foodiesclub.features.restaurants.data.mappers
+
+import com.rishabh.foodiesclub.features.restaurants.data.remote.dto.DealDto
+import com.rishabh.foodiesclub.features.restaurants.data.remote.dto.RestaurantDto
+import com.rishabh.foodiesclub.features.restaurants.domain.model.Deal
+import com.rishabh.foodiesclub.features.restaurants.domain.model.Restaurant
+
+fun RestaurantDto.toDomain(): Restaurant {
+    return Restaurant(
+        id = objectId,
+        name = name,
+        address = address1,
+        suburb = suburb,
+        cuisines = cuisines,
+        imageUrl = imageLink,
+        openTime = open,
+        closeTime = close,
+        deals = deals.map { it.toDomain() }
+    )
+}
+
+fun DealDto.toDomain(): Deal {
+    return Deal(
+        id = objectId,
+        discountPercent = discount.toIntOrNull() ?: 0,
+        isDineIn = dineIn.toBoolean(),
+        isLightningDeal = lightning.toBoolean(),
+        startTime = open,
+        endTime = close,
+        quantityLeft = qtyLeft.toIntOrNull() ?: 0
+    )
+}
