5e766a44f4e585cac0321837da423b3e4a90de08
Rishabh Bhatia <ojas14@gmail.com>
2025-08-03 15:42:46 +1000

    Add domain models

---
+++ app/src/main/java/com/rishabh/foodiesclub/features/restaurants/domain/model/Restaurant.kt
@@ -0,0 +1,13 @@
+package com.rishabh.foodiesclub.features.restaurants.domain.model
+
+data class Restaurant(
+    val id: String,
+    val name: String,
+    val address: String,
+    val suburb: String,
+    val cuisines: List<String>,
+    val imageUrl: String,
+    val openTime: String,
+    val closeTime: String,
+    val deals: List<Deal>
+)
