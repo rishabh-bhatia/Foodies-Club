df3f098968f25ea06caaf817de06abbefce23266
Rishabh Bhatia <ojas14@gmail.com>
2025-08-03 23:11:03 +1000

    Implement Navigation

--- app/src/main/java/com/rishabh/foodiesclub/MainActivity.kt
+++ app/src/main/java/com/rishabh/foodiesclub/MainActivity.kt
@@ -4,8 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.rishabh.foodiesclub.navigation.AppNavHost
 import com.rishabh.foodiesclub.core.ui.theme.FoodiesClubTheme
-import com.rishabh.foodiesclub.features.restaurants.ui.list.RestaurantListScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -15,7 +15,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FoodiesClubTheme {
-                RestaurantListScreen()
+                AppNavHost()
             }
         }
     }
