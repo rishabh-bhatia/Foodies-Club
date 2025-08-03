0cabb95d45f8a6d5e91190dca5c1d103573dd0c9
Rishabh Bhatia <ojas14@gmail.com>
2025-08-03 16:00:17 +1000

    Add UseCase

---
+++ app/src/main/java/com/rishabh/foodiesclub/features/restaurants/domain/usecase/GetRestaurantsUseCase.kt
@@ -0,0 +1,14 @@
+package com.rishabh.foodiesclub.features.restaurants.domain.usecase
+
+import com.rishabh.foodiesclub.features.restaurants.domain.model.Restaurant
+import com.rishabh.foodiesclub.features.restaurants.domain.repository.RestaurantRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetRestaurantsUseCase @Inject constructor(
+    private val repository: RestaurantRepository
+) {
+    operator fun invoke(): Flow<List<Restaurant>> {
+        return repository.getRestaurants()
+    }
+}
